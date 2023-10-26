c93f8a685809801b527515dca909bbc8e587040b
curiousclue89 <curiousclue89@gmail.com>
2023-10-26 02:39:23 -0300

    profile controller

--- CupcakeServer/Controllers/UserProfileController.cs
+++ CupcakeServer/Controllers/UserProfileController.cs
@@ -1,4 +1,5 @@
 ﻿using CupcakeServer.CQRS.Commands.Users;
+using CupcakeServer.CQRS.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,6 +18,19 @@ namespace CupcakeServer.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreateUserProfileCommand command)
         {
+            return Ok(await mediator.Send(command));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByUserId(int id)
+        {
+            return Ok(await mediator.Send(new GetUserProfileQuery { UserId = id }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateUserProfileCommand command)
+        {
+            command.UserId = id;
             return Ok(await mediator.Send(command));
         }
     }
